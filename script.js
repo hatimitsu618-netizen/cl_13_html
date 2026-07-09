2059a17d15bcb11cb529aef56748f5c2829eb0c4
hatimitsu618-netizen <296469437+hatimitsu618-netizen@users.noreply.github.com>
2026-07-09 16:30:43 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.chou').forEach(function (el) {
+  el.addEventListener('mouseover', function () {
+    
+    el.style.left = "50px";
+  });
+});
+
+document.querySelectorAll('.who').forEach(function (el) {
+  el.addEventListener('mouseover', function () {
+    
+    el.style.left = "50px";
+  });
+});
