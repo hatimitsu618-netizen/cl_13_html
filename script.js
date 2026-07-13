2115cedf26c21282f111126dbf6581c3c18f397b
hatimitsu618-netizen <296469437+hatimitsu618-netizen@users.noreply.github.com>
2026-07-13 20:32:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 document.querySelectorAll('.chou').forEach(function (el) {
   el.addEventListener('mouseover', function () {
-    
-    el.style.left = "50px";
+    el.style.left = (Math.random() * 750) + 100 + "px";
+    el.style.top = (Math.random() * 50) + 400 + "px";
   });
 });
 
 document.querySelectorAll('.who').forEach(function (el) {
+   el.style.left = (Math.random() * 450) + 370 + "px";
+   el.style.top = (Math.random() * 110) + 200 + "px";
+  
   el.addEventListener('mouseover', function () {
-    
-    el.style.left = "50px";
+    el.style.left = (Math.random() * 450) + 370 + "px";
+    el.style.top = (Math.random() * 110) + 200 + "px";
   });
 });
+
+
+
+
+
+
+window.addEventListener('mousemove', (e) => { console.clear(); console.log(`画面左端から: ${e.clientX}px, 画面上端から: ${e.clientY}px`); });
